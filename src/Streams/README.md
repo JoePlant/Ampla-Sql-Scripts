bc73bc91777839daef3899e85974f28f617bec0f
Joseph Plant <git@JosephPlant.com>
2014-11-19 16:28:02 +1000

    Added State Database scripts for number of streams

---
+++ src/Streams/README.md
@@ -0,0 +1,19 @@
+Runtime Stream Scripts
+===
+
+A set of scripts to output the number and size of the streams in the state database.
+
+----------
+
+###Stream.SampleCount.sql###
+
+Source: [SQL](Stream.SampleCount.sql)
+
+Outputs each of the streams in the state database showing their start and end time as well as the number of samples stored.
+
+Output:
+
+![SQL Output](../../images/database/Streams.SampleCount.png)
+
+----------
+
---
+++ src/Streams/Streams.SampleCount.sql
@@ -0,0 +1,56 @@
+/* -------------------------------------------------------------------
+--   Usage
+--		set @configDB = 'AmplaProject'
+----------------------------------------------------------------------*/
+
+declare @configDB varchar(25)
+declare @stateDB_override varchar(25)
+
+set @configDB = 'BMACrinum'    /* specify the config database here */
+
+/* uncomment following line to override the default state database */
+--set @stateDB_override = 'overide the state database'
+
+/*--------------------------------------------------------------------*/
+
+declare @sql nvarchar(1000)
+declare @stateDB varchar(25)
+
+set @stateDB = COALESCE(@stateDB_override, @configDB + 'State');
+
+set @sql = 
+'select 
+	streams.StreamId,
+	config.Name, 
+	streams.PropertyName,
+	config.TypeFullName,
+	samples.SampleCount,
+	((100.0 * samples.SampleCount) / summary.TotalCount) as Percentage,
+	' + @stateDB +'.dbo.udfTicksToDateTime(streams.StartTimeTicks) as StartTime,
+	' + @stateDB +'.dbo.udfTicksToDateTime(streams.EndTimeTicks) as EndTime
+from 
+	(select count(1) as TotalCount from ' + @stateDB +'.dbo.SampleStoreSample) summary,
+	' + @stateDB +'.dbo.SampleStoreSampleStream streams
+	left join (
+		select StreamId, count(1) as SampleCount
+		from '+ @stateDB +'.dbo.SampleStoreSample
+		group by StreamId
+	) samples 
+	on 
+		streams.StreamId = samples.StreamId
+	left join (
+		select items.ID, items.[Name], types.TypeFullName
+		from ' + @configDB + '.dbo.Items items
+		inner join ' + @configDB + '.dbo.ItemTypes types on 
+		items.TypeId = types.ID
+	) config 
+	on 
+		config.ID = streams.ItemId
+order by 
+	samples.SampleCount desc,
+	config.Name desc'
+
+--print @sql
+exec sp_executesql @sql
+
+
