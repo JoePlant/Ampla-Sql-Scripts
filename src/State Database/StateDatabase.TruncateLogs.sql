91288c310aa613fda273a0da48e67dd0b3d47cf3
Joseph Plant <git@JosephPlant.com>
2015-01-15 16:10:09 +1000

    Updated logging for deleted records

--- src/State Database/StateDatabase.TruncateLogs.sql
+++ src/State Database/StateDatabase.TruncateLogs.sql
@@ -1,3 +1,9 @@
+/*
+
+	Ampla State Database Script to manage the size of the Log and Category Log tables.
+
+*/
+
 declare @utcNow datetime
 declare @weeksOld int
 declare @countLogs int
@@ -11,11 +17,13 @@ from
 		from dbo.Log
 	) logSummary
 
-
-
-
 if (@weeksOld > 4)
 begin
+
+	declare @categoryLogDeleted int
+	declare @scenarioDeleted int
+	declare @logDeleted int
+	
 	print 'Log table has ' + cast(@countLogs as varchar(10)) + ' rows'
 	print 'Oldest log record is ' + cast(@weeksOld as varchar(10)) + ' weeks old'
 	
@@ -23,28 +31,36 @@ begin
 	set @expiresAt = DATEADD(WK, -1 * (@weeksOld - 1), @utcNow) 
 	print 'Deleting log entries - Expiry date (UTC): ' + cast(@expiresAt as varchar(20))
 
-
 	print '-- Deleting records from CategoryLog'
 	DELETE dbo.CategoryLog
-	--select CategoryLogId
 	from dbo.CategoryLog
 	INNER JOIN dbo.Log
 		ON Log.LogId = CategoryLog.LogId	
 	WHERE Log.Timestamp < @expiresAt
+	-- get the number of records deleted
+	set @categoryLogDeleted = @@ROWCOUNT
 
 	print '-- Deleting records from Scenario'
 	DELETE dbo.Scenario
-	--select *
 	from dbo.Scenario
 	INNER JOIN dbo.Log
 		ON Log.InstanceId = Scenario.InstanceId	
 	WHERE Log.Timestamp < @expiresAt
+	-- get the number of records deleted
+	set @scenarioDeleted = @@ROWCOUNT
 
 	print '-- Deleting records from Log'
 	DELETE dbo.Log
-	--select *
 	from dbo.Log
 	WHERE Log.Timestamp < @expiresAt 
+	-- get the number of records deleted
+	set @logDeleted = @@ROWCOUNT
+
+	print '*** [Summary] ***'
+	print '***  Deleted ' + cast(@logDeleted as varchar(10)) + ' rows from the Log table'
+	print '***  Deleted ' + cast(@categoryLogDeleted as varchar(10)) + ' rows from the CategoryLog table'
+	print '***  Deleted ' + cast(@scenarioDeleted as varchar(10)) + ' rows from the Scenario table'
+
 end 
 else
 begin
