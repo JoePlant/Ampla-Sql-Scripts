e6549d934ae2b2278ed4a15d64fef43754e09978
Joseph Plant <git@JosephPlant.com>
2015-01-16 10:53:51 +1000

    Updated Database.BackupScript

--- src/Database/Database.BackupScript.sql
+++ src/Database/Database.BackupScript.sql
@@ -1,16 +1,28 @@
+/*
+	Database.BackupScript.sql
+	Generates a set of backup scripts for Ampla Databases with the date in the file name
+	
+	Adjust the name of the database as required.
+	Script can be modified to execute directly
+*/
+
 declare @date varchar (10)
 declare @sqlFormat varchar(1000)
 declare @sql varchar(max)
+declare @backupDir varchar(100)
 
 set @date = convert(varchar(10),getDate(),120)
 
+set @backupDir = 'C:\Backups\Databases\'
+
 set @sqlFormat = 'BACKUP DATABASE [' 
 			+ '<<db-name>>' 
-			+ '] TO  DISK = N''E:\Backup\AmplaProject\' 
+			+ '] TO DISK = N''' 
+			+ @backupDir
 			+ @date 
 			+ ' ' 
 			+ '<<db-name>>'
-			+ '.bak'' WITH NOFORMAT, INIT,  NAME = N''' 
+			+ '.bak'' WITH NOFORMAT, INIT, NAME = N''' 
 			+ '<<db-name>>'
 			+ '-Full Backup'', SKIP, NOREWIND, NOUNLOAD,  STATS = 10'
 
