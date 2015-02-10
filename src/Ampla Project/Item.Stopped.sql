2bbf2852ad873ea4bf8fa0c43173973ccfaedb54
Joseph Plant <git@JosephPlant.com>
2015-02-10 18:05:47 +1000

    Script for Stopped items in Project

---
+++ src/Ampla Project/Item.Stopped.sql
@@ -0,0 +1,20 @@
+/* 
+	Item.Stopped.sql
+*/
+WITH Item_CTE(ID, FullName, TypeID, Depth)
+AS
+(
+	SELECT ID, Cast(Name as VARCHAR(MAX)) as FullName, TypeID, 1 as Depth
+	FROM Items
+	WHERE ParentID = '00000000-0000-0000-0000-000000000000'
+	UNION ALL
+	SELECT item.ID,  Cast(cte.FullName + '.' + item.Name as VARCHAR(MAX)) as FullName, item.TypeID, cte.Depth + 1 as Depth
+	FROM Items as item
+	INNER JOIN Item_CTE as cte
+		ON item.ParentID = cte.ID
+)
+
+select cte.FullName as [Stopped Items]
+from ItemProperties ip
+inner join Item_CTE cte on ip.ItemID = cte.ID
+where PropertyName = 'StartupMode'
