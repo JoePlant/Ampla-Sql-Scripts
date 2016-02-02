68bb6ba483cef9300803e602d6af0e814dbe6b01
Joseph Plant <git@JosephPlant.com>
2016-02-03 09:31:18 +1000

    Add Error messages to Streams

--- src/Streams/Streams.Orphaned.sql
+++ src/Streams/Streams.Orphaned.sql
@@ -2,14 +2,16 @@
 --  Determines if there is any orphaned streams in the state database 
 --  and creates a SQL Script to remove them
 --
---   Usage
---		set @configDB = 'AmplaProject'
-----------------------------------------------------------------------*/
+--   Usage 
+--		set @configDB = DB_NAME()		/* to use the current database */
+--		set @configDB = 'AmplaProject'	/* or specify the database     */
+---------------------------------------------------------------------- */
 
-declare @configDB varchar(25)
-declare @stateDB_override varchar(25)
+declare @configDB varchar(50)
+declare @stateDB_override varchar(50)
 
-set @configDB = 'AmplaProject'    /* specify the config database here */
+set @configDB = DB_NAME()			/* use the current database */
+-- set @configDB = 'AmplaProject'	/* or specify the config database here */
 
 /* uncomment following line to override the default state database */
 --set @stateDB_override = 'overide the state database'
@@ -17,9 +19,32 @@ set @configDB = 'AmplaProject'    /* specify the config database here */
 /*--------------------------------------------------------------------*/
 
 declare @sql nvarchar(max)
-declare @stateDB varchar(25)
+declare @stateDB varchar(50)
 
 set @stateDB = COALESCE(@stateDB_override, @configDB + 'State');
+
+--print 'ConfigDB: ' + @configDB
+--print 'StateDB: ' + @stateDB
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = @configDB)
+BEGIN
+	print '** ERROR - Unable to find Ampla Config Database : [' + @configDB + ']'
+END
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = @stateDB)
+BEGIN
+	PRINT '** ERROR - Unable to find Ampla State Database : [' + @stateDB + ']'
+END 
+
+IF OBJECT_ID(@configDB +'.dbo.Items', 'U') IS NULL
+BEGIN 
+	PRINT '** ERROR - Ampla Config Database : [' + @configDB + '] is not a valid Ampla config database.'
+END
+
+IF OBJECT_ID(@stateDB +'.dbo.SampleStoreSample', 'U') IS NULL
+BEGIN 
+	PRINT '** ERROR - Ampla State Database : [' + @stateDB + '] is not a valid Ampla State database.'
+END
 
 set @sql = 
 '
@@ -28,25 +53,25 @@ select
 	streams.ItemId,
 	streams.PropertyName,
 	COALESCE(samples.SampleCount, 0) as SampleCount,
-	' + @stateDB + '.dbo.udfTicksToDateTime(streams.StartTimeTicks) as StartTime,
-	' + @stateDB + '.dbo.udfTicksToDateTime(streams.EndTimeTicks) as EndTime,
+	[' + @stateDB + '].dbo.udfTicksToDateTime(streams.StartTimeTicks) as StartTime,
+	[' + @stateDB + '].dbo.udfTicksToDateTime(streams.EndTimeTicks) as EndTime,
 	CASE WHEN COALESCE(samples.SampleCount,0) > 0 THEN 
-	''DELETE FROM ' + @stateDB + '.dbo.SampleStoreSample WHERE StreamId='' + cast(streams.StreamId as varchar(10)) + CHAR(13) 
+	''DELETE FROM [' + @stateDB + '].dbo.SampleStoreSample WHERE StreamId='' + cast(streams.StreamId as varchar(10)) + CHAR(13) 
 	ELSE '''' END + 
-	''DELETE FROM ' + @stateDB + '.dbo.SampleStoreSampleStream WHERE StreamId='' + cast(streams.StreamId as varchar(10)) 
+	''DELETE FROM [' + @stateDB + '].dbo.SampleStoreSampleStream WHERE StreamId='' + cast(streams.StreamId as varchar(10)) 
 	as [Delete SQL]
 from 
-	' + @stateDB + '.dbo.SampleStoreSampleStream streams
+	[' + @stateDB + '].dbo.SampleStoreSampleStream streams
 	left join (
 		select StreamId, count(1) as SampleCount
-		from ' + @stateDB + '.dbo.SampleStoreSample
+		from [' + @stateDB + '].dbo.SampleStoreSample
 		group by StreamId
 	) samples 
 	on 
 		streams.StreamId = samples.StreamId
 where ItemId not in (
 	select Id 
-	from ' + @configDB + '.dbo.Items
+	from [' + @configDB + '].dbo.Items
 	)		
 
 order by 
--- src/Streams/Streams.SampleCount.sql
+++ src/Streams/Streams.SampleCount.sql
@@ -1,12 +1,16 @@
 /* -------------------------------------------------------------------
---   Usage
---		set @configDB = 'AmplaProject'
-----------------------------------------------------------------------*/
+--  Matches the streams in the state database with the items in the project 
+-- 
+--   Usage 
+--		set @configDB = DB_NAME()		/* to use the current database */
+--		set @configDB = 'AmplaProject'	/* or specify the database     */
+---------------------------------------------------------------------- */
 
-declare @configDB varchar(25)
-declare @stateDB_override varchar(25)
+declare @configDB varchar(50)
+declare @stateDB_override varchar(50)
 
-set @configDB = 'AmplaProject'    /* specify the config database here */
+set @configDB = DB_NAME()			/* use the current database */
+-- set @configDB = 'AmplaProject'	/* or specify the config database here */
 
 /* uncomment following line to override the default state database */
 --set @stateDB_override = 'overide the state database'
@@ -14,21 +18,43 @@ set @configDB = 'AmplaProject'    /* specify the config database here */
 /*--------------------------------------------------------------------*/
 
 declare @sql nvarchar(max)
-declare @stateDB varchar(25)
+declare @stateDB varchar(50)
 
 set @stateDB = COALESCE(@stateDB_override, @configDB + 'State');
 
+--print 'ConfigDB: ' + @configDB
+--print 'StateDB: ' + @stateDB
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = @configDB)
+BEGIN
+	print '** ERROR - Unable to find Ampla Config Database : [' + @configDB + ']'
+END
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = @stateDB)
+BEGIN
+	PRINT '** ERROR - Unable to find Ampla State Database : [' + @stateDB + ']'
+END 
+
+IF OBJECT_ID(@configDB +'.dbo.Items', 'U') IS NULL
+BEGIN 
+	PRINT '** ERROR - Ampla Config Database : [' + @configDB + '] is not a valid Ampla config database.'
+END
+
+IF OBJECT_ID(@stateDB +'.dbo.SampleStoreSample', 'U') IS NULL
+BEGIN 
+	PRINT '** ERROR - Ampla State Database : [' + @stateDB + '] is not a valid Ampla State database.'
+END
 set @sql = 
 '
 WITH Item_CTE(ID, FullName, TypeID, Depth)
 AS
 (
 	SELECT ID, Cast(Name as VARCHAR(MAX)) as FullName, TypeID, 1 as Depth
-	FROM ' + @configDB + '.dbo.Items
+	FROM [' + @configDB + '].dbo.Items
 	WHERE ParentID = ''00000000-0000-0000-0000-000000000000''
 	UNION ALL
 	SELECT item.ID,  Cast(cte.FullName + ''.'' + item.Name as VARCHAR(MAX)) as FullName, item.TypeID, cte.Depth + 1 as Depth
-	FROM ' + @configDB + '.dbo.Items as item
+	FROM [' + @configDB + '].dbo.Items as item
 	INNER JOIN Item_CTE as cte
 		ON item.ParentID = cte.ID
 )
@@ -40,14 +66,14 @@ select
 	config.TypeFullName,
 	samples.SampleCount,
 	((100.0 * samples.SampleCount) / summary.TotalCount) as Percentage,
-	' + @stateDB +'.dbo.udfTicksToDateTime(streams.StartTimeTicks) as StartTime,
-	' + @stateDB +'.dbo.udfTicksToDateTime(streams.EndTimeTicks) as EndTime
+	[' + @stateDB +'].dbo.udfTicksToDateTime(streams.StartTimeTicks) as StartTime,
+	[' + @stateDB +'].dbo.udfTicksToDateTime(streams.EndTimeTicks) as EndTime
 from 
-	(select count(1) as TotalCount from ' + @stateDB +'.dbo.SampleStoreSample) summary,
-	' + @stateDB +'.dbo.SampleStoreSampleStream streams
+	(select count(1) as TotalCount from [' + @stateDB +'].dbo.SampleStoreSample) summary,
+	[' + @stateDB +'].dbo.SampleStoreSampleStream streams
 	left join (
 		select StreamId, count(1) as SampleCount
-		from '+ @stateDB +'.dbo.SampleStoreSample
+		from ['+ @stateDB +'].dbo.SampleStoreSample
 		group by StreamId
 	) samples 
 	on 
@@ -55,7 +81,7 @@ from
 	left join (
 		SELECT items.ID, items.FullName, items.Depth, types.TypeFullName
 		from Item_CTE items
-		inner join ' + @configDB + '.dbo.ItemTypes types
+		inner join [' + @configDB + '].dbo.ItemTypes types
 		on items.TypeID = types.ID
 	) config 
 	on 
